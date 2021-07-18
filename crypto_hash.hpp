b10d09c11feeeea1e2eba9e059e15552abc707a1
abbbaf <abbbaf@gmail.com>
2021-07-19 00:02:35 +0300

    Add files via upload

--- crypto_hash.hpp
+++ crypto_hash.hpp
@@ -12,10 +12,10 @@ template<typename H, int N>
 class CryptoHash {
     private:
         array<uint32_t,N> hashed_value;
-        const int size;
+        const size_t size;
 
     protected:
-        constexpr CryptoHash(array<uint32_t,N> _hashed_value, int _size = 0) :
+        constexpr CryptoHash(array<uint32_t,N> _hashed_value, size_t _size = 0) :
             hashed_value(_hashed_value),
             size(_size ? _size : hashed_value.size()) {}
 
@@ -23,27 +23,37 @@ class CryptoHash {
 
         template<typename T, int N2>
         bool operator ==(const CryptoHash<T,N2> &second_hash) const {
-          for (int i = 0; i < size; i++) {
-            if (hashed_value[i] != second_hash[i]) return false;
-          }
-          return true;
+            for (int i = 0; i < size; i++) {
+                if (hashed_value[i] != second_hash[i]) return false;
+            }
+            return true;
         }
 
         template<typename T, int N2>
         bool operator !=(const CryptoHash<T,N2> &second_hash) const {
-          return !(this == second_hash);
+            return !(this == second_hash);
         }
         
+
         uint32_t operator[](int index) const {
-          return hashed_value[index];
+            return hashed_value[index];
+        }
+
+        size_t length() const {
+            return size;
         }
 
         friend ostream& operator<<(ostream& os, const CryptoHash<H,N>& hash) {
             for (int i = 0; i < hash.size; i++ )
                 os << std::hex << setfill('0') << setw(8) << hash[i]; 
             return os;
-    }
+        }
+
+
 
 };
+
+
+
 
 #endif
